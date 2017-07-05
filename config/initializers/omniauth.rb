564967b6ff9a311fe6a7a70a1346c49af2650937
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-05 14:11:45 -0600

    starts to change scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['client_id'], ENV['client_secret'], scope: 'playlist-read-private user-read-private user-read-email'
+  provider :spotify, ENV['client_id'], ENV['client_secret'], scope: 'playlist-modify-public playlist-modify-private user-read-email'
 end
