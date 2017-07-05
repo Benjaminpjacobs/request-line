9eb1c571c4d9b5ee7afa1a17af365f2e3f75eefe
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-05 14:02:04 -0600

    creates users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/spotify', as: :spotify_login
+  get '/auth/spotify/callback', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    uid "MyString"
+    name "MyString"
+    email "MyString"
+    token "MyString"
+    refresh_token "MyString"
+  end
+end
